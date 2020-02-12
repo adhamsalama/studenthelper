865b0c15b4067e93763994da7152063c916633a5
adham <adhamsalama666@gmail.com>
2020-02-13 01:57:05 +0200

    Changed Service Worker Settings

--- static/pwabuilder-sw.js
+++ static/pwabuilder-sw.js
@@ -1,9 +1,14 @@
 // This is the "Offline page" service worker
 
 const CACHE = "pwabuilder-page";
-
+const assets = [
+    '/',
+    '/schedule',
+    '/profile',
+    './'
+]
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
+//const offlineFallbackPage = "index.html";
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
@@ -13,11 +18,12 @@ self.addEventListener("install", function (event) {
     caches.open(CACHE).then(function (cache) {
       console.log("[PWA Builder] Cached offline page during install");
 
-      if (offlineFallbackPage === "index.html") {
+      /*if (offlineFallbackPage === "index.html") {
         return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
       }
 
-      return cache.add(offlineFallbackPage);
+      return cache.add(offlineFallbackPage);*/
+      cache.addAll(assets);
     })
   );
 });
