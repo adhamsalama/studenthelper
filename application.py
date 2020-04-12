458716c56fed7e6397a473534e3256db4cbe3877
adham <adhamsalama666@gmail.com>
2020-04-12 10:31:36 +0200

    Changed session-permanent to true

--- application.py
+++ application.py
@@ -18,7 +18,7 @@ from others.routes import others
 app = Flask(__name__)
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
