3679f5e98949d5d2cf33903a1884e36799c042cc
adhamsalama <adhamsalama666@gmail.com>
2020-12-11 17:11:51 +0200

    Added event listener

--- static/due.js
+++ static/due.js
@@ -1,31 +1,35 @@
 function verify_due() {
     return confirm("Are you sure you want to delete this due?");
 }
-let d1 = new Date(); //"now"
-let deadlines = document.querySelectorAll(".deadline");
-let dates = document.querySelectorAll(".time-left");
-for (let i = 0; i < deadlines.length; i++) {
-    let d2 = new Date(deadlines[i].innerHTML);
-    let diffTime = d2 - d1;
-    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    if (diffDays == 0) {
-        dates[i].innerHTML = "Today";
-    }
-    else if (diffDays == 1) {
-        dates[i].innerHTML = "Tomorrow";
-    }
-    else if (diffDays == -1) {
-        dates[i].innerHTML = "Yesterday";
-    }
-    else if (diffDays % 7 == 0) {
-        if (diffDays == 7) {
-            dates[i].innerHTML = "1 week";
+document.addEventListener("DOMContentLoaded", calculate_time_left);
+
+function calculate_time_left() {
+    let d1 = new Date(); //"now"
+    let deadlines = document.querySelectorAll(".deadline");
+    let dates = document.querySelectorAll(".time-left");
+    for (let i = 0; i < deadlines.length; i++) {
+        let d2 = new Date(deadlines[i].innerHTML);
+        let diffTime = d2 - d1;
+        let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        if (diffDays == 0) {
+            dates[i].innerHTML = "Today";
+        }
+        else if (diffDays == 1) {
+            dates[i].innerHTML = "Tomorrow";
+        }
+        else if (diffDays == -1) {
+            dates[i].innerHTML = "Yesterday";
+        }
+        else if (diffDays % 7 == 0) {
+            if (diffDays == 7) {
+                dates[i].innerHTML = "1 week";
+            }
+            else {
+                dates[i].innerHTML = diffDays / 7 + " weeks"
+            }
         }
         else {
-            dates[i].innerHTML = diffDays / 7 + " weeks"
+            dates[i].innerHTML = diffDays + " days";
         }
-    }
-    else {
-        dates[i].innerHTML = diffDays + " days";
     }
 }
--- static/subject_time.js
+++ static/subject_time.js
@@ -1,15 +1,20 @@
-let tds = document.querySelectorAll("td");
-for (let i = 0; i < tds.length; i++) {
-    tds[i].style.verticalAlign = "middle";
-}
-let time = document.querySelectorAll(".time");
-for (let i = 0, n = time.length; i < n; i++) {
-    let minutes = parseInt(time[i].innerHTML[3]) * 10 + parseInt(time[i].innerHTML[4]);
-    if (minutes < 10) {
-        minutes = "0" + minutes;
+document.addEventListener("DOMContentLoaded", format_subject_time);
+
+function format_subject_time() {
+    console.log("running subject time");
+    let tds = document.querySelectorAll("td");
+    for (let i = 0; i < tds.length; i++) {
+        tds[i].style.verticalAlign = "middle";
     }
-    let hours = parseFloat(time[i].innerHTML);
-    let suffix = hours >= 12 ? " PM" : " AM";
-    hours = ((hours + 11) % 12 + 1);
-    time[i].innerHTML = hours + ":" + minutes + suffix;
+    let time = document.querySelectorAll(".time");
+    for (let i = 0, n = time.length; i < n; i++) {
+        let minutes = parseInt(time[i].innerHTML[3]) * 10 + parseInt(time[i].innerHTML[4]);
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        let hours = parseFloat(time[i].innerHTML);
+        let suffix = hours >= 12 ? " PM" : " AM";
+        hours = ((hours + 11) % 12 + 1);
+        time[i].innerHTML = hours + ":" + minutes + suffix;
+    }
 }
